5393358699ca8ff0d7f0e991a7ed5e524be35ebd
Reduzer <83287196+Reduzer@users.noreply.github.com>
2024-05-28 16:01:15 +0200

    Base structure

---
+++ PasswordChecker/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PasswordChecker
+{
+    public static class PasswordChecker
+    {
+        public static void Main()
+        {
+
+        }
+
+        public static bool checkGeneratedPassword()
+        {
+            return false;
+        }
+    }
+}
---
+++ PasswordGenerator/src/check.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PasswordChecker;
+
+namespace PasswordGenerator.src
+{
+    internal class check
+    {
+        public bool checkPW()
+        {
+            return true; 
+        }
+
+
+        
+    }
+}
